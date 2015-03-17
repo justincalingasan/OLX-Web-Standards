5874128681c49fb699292db64c1e5a0d8449fe49
Ken Banzon <kabanzon@gmail.com>
2015-03-18 02:53:42 +0800

    Initial commit.

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="/css/style.css">
+	</head>
+	<body>
+		<section class="row">
+			<div class="small-12 medium-8 medium-offset-2">
+				<h1>Heading 1</h1>
+				<h2>Heading 2</h2>
+				<h3>Heading 3</h3>
+				<h4>Heading 4</h4>
+				<h5>Heading 5</h5>
+				<h6>Heading 6</h6>
+				<p>Body copy</p>
+				<a href="">Link</a>
+			</div>
+			<hr>
+			<div class="small-12 medium-8 medium-offset-2">
+				<a href="" class="button alert">Button</a>
+				<a href="" class="button error">Error</a>
+				<a href="" class="button warning">Warning</a>
+				<a href="" class="button success">Success</a>
+				<a href="" class="button neutral">Neutral</a>
+				<a href="" class="button white">White</a>
+				<a href="" class="button disabled">Disabled</a>
+				<br>
+				<br>
+				<a href="" class="button">Regular</a>
+				<a href="" class="button small">Small</a>
+				<a href="" class="button tiny">Tiny</a>
+				<br>
+				<br>
+				<a href="" class="button strong">Regular Strong</a>
+				<a href="" class="button small strong">Small Strong</a>
+				<a href="" class="button tiny strong">Tiny Strong</a>
+				<br>
+				<br>
+				<a href="" class="button full">Full Width</a>
+				<br>
+				<a href="" class="button full strong">Full Width Strong</a>
+			</div>
+			<hr>
+
+		</section>
+	</body>
+</html>
