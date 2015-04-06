f19a8c2897102c35aab35c9394af10a4ca6373cd
Ken Banzon <kabanzon@gmail.com>
2015-04-06 15:20:46 +0800

    Added dropdowns, search bar, content box and sample cards

--- index.php
+++ index.php
@@ -91,6 +91,192 @@
 				</div>
 			</div>
 			<hr>
+			<h1>Dropdowns</h1>
+			<div class="small-12 medium-8 medium-offset-2">
+				<div class="dropdown">
+					<a href="" class="button dropdown-toggle">Dropdown &#9662;</a>
+					<ul class="dropdown-menu">
+						<li><a href="">Option A</a></li>
+						<li><a href="">Option B</a></li>
+					</ul>
+				</div>
+			</div>
+			<hr>
 		</section>
+		<section class="row">
+			<h1>Search Bar</h1>
+			<div class="small-12">
+				<div class="search-bar clearfix">
+					<div class="search-field small-9 columns">
+						<span>&#128270;</span>
+						<input type="text" placeholder="Ano hanap mo?">
+						<div class="dropdown">
+							<a href="" class="button small dropdown-toggle">in <strong>Current Location</strong> &#9662;</a>
+							<ul class="dropdown-menu small">
+								<li><a href="" class="active">Current Location</a></li>
+								<li><a href="">Metro Manila</a></li>
+								<li><a href="">Batangas</a></li>
+								<li><a href="">Benguet</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="small-3 columns">
+						<a href="" class="button white full">Search</a>
+					</div>
+				</div>
+			</div>
+			<hr>
+		</section>
+		<section class="row">
+			<h1>Content Box</h1>
+			<div class="small-12">
+				<div class="card small-6 medium-4 large-3">
+					<div class="card-header">
+						Hello!
+					</div>
+					<div class="card-body">
+						<p>Body text here.</p>
+						<a href="" class="button success">Yay!</a>
+					</div>
+				</div>
+				<div class="card small-6 medium-6 large-4">
+					<div class="card-header">
+						<small>22 views</small>
+					</div>
+					<div class="card-body">
+						<div class="table-data">
+							<div class="data-set">
+								<div class="label"><strong>Price</strong></div>
+								<div><strong>Php 1,800.00</strong></div>
+							</div>
+							<div class="data-set">
+								<div class="label">Contact Number</div>
+								<div><a href="" class="button success"><strong>0917-588-05xx</strong></a></div>
+							</div>
+							<div class="data-set">
+								<div class="label">Private Message</div>
+								<div><a href="" class="button tiny white">Send this seller a message</a></div>
+							</div>
+							<div class="data-set">
+								<div class="label">Date Posted</div>
+								<div>March 28, 2015</div>
+							</div>
+							<div class="data-set">
+								<div class="label">Condition</div>
+								<div>2nd Hand (used)</div>
+							</div>
+							<div class="data-set">
+								<div class="label">Location</div>
+								<div><a href="">Taguig City, Metro Manila</a></div>
+							</div>
+						</div>
+					</div>
+					<div class="card-actions">
+						<a href="" class="button white small full">More Ad Details</a>
+					</div>
+				</div>
+				<div class="card small-6 medium-4 large-3">
+					<div>
+						<img src="http://placehold.it/300x200" alt="">
+					</div>
+					<div class="card-body item">
+						<div class="item-name"><strong><a href="">iPhone 5 32GB Black very smooth</a></strong></div>
+						<div class="item-price"><strong>Php 12,990</strong></div>
+						<div class="item-details">
+							<small>Posted 6 minutes ago</small>
+							<small>Paranaque City, Metro Manila</small>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="row">
+			<hr>
+			<h1>Sample Listing Area</h1>
+			<div class="card small-6 medium-3 large-3">
+				<div>
+					<img src="http://placehold.it/300x200" alt="">
+				</div>
+				<div class="card-body item">
+					<div class="item-name"><strong><a href="">iPhone 5 32GB Black very smooth</a></strong></div>
+					<div class="item-price"><strong>Php 12,990</strong></div>
+					<div class="item-details">
+						<small>Posted 6 minutes ago</small>
+						<small>Paranaque City, Metro Manila</small>
+					</div>
+				</div>
+			</div>
+			<div class="card small-6 medium-3 large-3">
+				<div>
+					<img src="http://placehold.it/300x200" alt="">
+				</div>
+				<div class="card-body item">
+					<div class="item-name"><strong><a href="">iPhone 5 32GB Black very smooth</a></strong></div>
+					<div class="item-price"><strong>Php 12,990</strong></div>
+					<div class="item-details">
+						<small>Posted 6 minutes ago</small>
+						<small>Paranaque City, Metro Manila</small>
+					</div>
+				</div>
+			</div>
+			<div class="card small-6 medium-3 large-3">
+				<div>
+					<img src="http://placehold.it/300x200" alt="">
+				</div>
+				<div class="card-body item">
+					<div class="item-name"><strong><a href="">iPhone 5 32GB Black very smooth</a></strong></div>
+					<div class="item-price"><strong>Php 12,990</strong></div>
+					<div class="item-details">
+						<small>Posted 6 minutes ago</small>
+						<small>Paranaque City, Metro Manila</small>
+					</div>
+				</div>
+			</div>
+			<div class="card small-6 medium-3 large-3">
+				<div>
+					<img src="http://placehold.it/300x200" alt="">
+				</div>
+				<div class="card-body item">
+					<div class="item-name"><strong><a href="">iPhone 5 32GB Black very smooth</a></strong></div>
+					<div class="item-price"><strong>Php 12,990</strong></div>
+					<div class="item-details">
+						<small>Posted 6 minutes ago</small>
+						<small>Paranaque City, Metro Manila</small>
+					</div>
+				</div>
+			</div>
+			<div class="card small-6 medium-3 large-3">
+				<div>
+					<img src="http://placehold.it/300x200" alt="">
+				</div>
+				<div class="card-body item">
+					<div class="item-name"><strong><a href="">iPhone 5 32GB Black very smooth</a></strong></div>
+					<div class="item-price"><strong>Php 12,990</strong></div>
+					<div class="item-details">
+						<small>Posted 6 minutes ago</small>
+						<small>Paranaque City, Metro Manila</small>
+					</div>
+				</div>
+			</div>
+			<div class="card small-6 medium-3 large-3">
+				<div>
+					<img src="http://placehold.it/300x200" alt="">
+				</div>
+				<div class="card-body item">
+					<div class="item-name"><strong><a href="">iPhone 5 32GB Black very smooth</a></strong></div>
+					<div class="item-price"><strong>Php 12,990</strong></div>
+					<div class="item-details">
+						<small>Posted 6 minutes ago</small>
+						<small>Paranaque City, Metro Manila</small>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		
+
+		<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+		<script src="js/ui.js"></script>
+
 	</body>
 </html>
